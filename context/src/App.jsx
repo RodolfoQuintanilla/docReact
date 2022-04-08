076da3d6d1f5d5c21c63462fbe075afb8fd805fd
RodolfoQuintanilla <rqm@outlook.es>
2022-04-07 22:51:04 -0500

    uno

---
+++ context/src/App.jsx
@@ -0,0 +1,13 @@
+import AppSeguro from "./components/AppSeguro"
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <AppSeguro/>
+    </div>
+  )
+}
+
+export default App
---
+++ context/src/components/AppSeguro.jsx
@@ -0,0 +1,18 @@
+import Formulario from "./Formulario";
+
+
+const AppSeguro = () => {
+    return (
+        <>
+      <header>
+          <h1>Titulo</h1>
+      </header>
+      <div class="container"> 
+        <Formulario/>   
+      </div>
+     
+        </>
+    );
+};
+
+export default AppSeguro;
